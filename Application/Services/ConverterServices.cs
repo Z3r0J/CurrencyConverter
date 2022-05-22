1ccde462d5f570dc5e2b95359106c4e567bd8f3e
Jean Carlos Reyes Encarnación <20210546@itla.edu.do>
2022-05-21 23:33:55 -0400

    Currency Logic Finished Yet

--- Application/Services/ConverterServices.cs
+++ Application/Services/ConverterServices.cs
@@ -46,6 +46,78 @@ namespace Application.Services
                     break;
                 #endregion
 
+                #region UnitedStateDollar
+                case (int)EnumCurrency.Currency.Dollar:
+                    switch (vm.DestinationCurrency)
+                    {
+                        case (int)EnumCurrency.Currency.Peso:
+                            response = new()
+                            {
+                                ActualCurrency = "USD",
+                                DestinationCurrency = "DOP",
+                                Total = vm.Amount * 55
+                            };
+                            return response;
+
+                        case (int)EnumCurrency.Currency.Dollar:
+                            response = new()
+                            {
+                                ActualCurrency = "USD",
+                                DestinationCurrency = "USD",
+                                Total = vm.Amount * 1
+                            };
+                            return response;
+
+                        case (int)EnumCurrency.Currency.Euro:
+                            response = new()
+                            {
+                                ActualCurrency = "USD",
+                                DestinationCurrency = "EUR",
+                                Total = vm.Amount * 0.94
+                            };
+                            return response;
+                        default:
+                            break;
+                    }
+                    break;
+                #endregion
+
+                #region Euro
+                case (int)EnumCurrency.Currency.Euro:
+                    switch (vm.DestinationCurrency)
+                    {
+                        case (int)EnumCurrency.Currency.Peso:
+                            response = new()
+                            {
+                                ActualCurrency = "EUR",
+                                DestinationCurrency = "DOP",
+                                Total = vm.Amount * 58
+                            };
+                            return response;
+
+                        case (int)EnumCurrency.Currency.Dollar:
+                            response = new()
+                            {
+                                ActualCurrency = "EUR",
+                                DestinationCurrency = "USD",
+                                Total = vm.Amount * 1.05
+                            };
+                            return response;
+
+                        case (int)EnumCurrency.Currency.Euro:
+                            response = new()
+                            {
+                                ActualCurrency = "EUR",
+                                DestinationCurrency = "EUR",
+                                Total = vm.Amount * 1
+                            };
+                            return response;
+                        default:
+                            break;
+                    }
+                    break;
+                #endregion
+
                 default:
                     break;
             }
--- CurrencyConverter/Controllers/ConverterController.cs
+++ CurrencyConverter/Controllers/ConverterController.cs
@@ -1,4 +1,6 @@
-﻿using Application.ViewModel;
+﻿using Application;
+using Application.Services;
+using Application.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,17 +12,19 @@ namespace CurrencyConverter.Controllers
     public class ConverterController : Controller
     {
         private readonly CurrencyInformationViewModel info;
+        private readonly ConverterServices services;
         public ConverterController()
         {
             info = new();
+            services = new();
         }
         public IActionResult Index()
         {
             return View(info);
         }
         public PartialViewResult GetResponse(CurrencyInformationViewModel vm) {
-
-            return PartialView("_GetResponse",vm);
+            var response = services.Converter(vm);
+            return PartialView("_GetResponse",response);
         }
     }
 }
