4d4be58dc3f6da75071ae3acaf3cd561b5d0034c
Jean Carlos Reyes Encarnación <20210546@itla.edu.do>
2022-05-21 22:55:42 -0400

    Add project files.

---
+++ Application/ViewModel/CurrencyInformationViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.ViewModel
+{
+    public class CurrencyInformationViewModel
+    {
+        public int ActualCurrency { get; set; }
+        public int DestinationCurrency { get; set; }
+        public double Amount { get; set; }
+
+        public double Total { get; set; } = 0;
+    }
+}
---
+++ CurrencyConverter/Controllers/ConverterController.cs
@@ -0,0 +1,26 @@
+﻿using Application.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CurrencyConverter.Controllers
+{
+    public class ConverterController : Controller
+    {
+        private readonly CurrencyInformationViewModel info;
+        public ConverterController()
+        {
+            info = new();
+        }
+        public IActionResult Index()
+        {
+            return View(info);
+        }
+        public PartialViewResult GetResponse(CurrencyInformationViewModel vm) {
+
+            return PartialView("_GetResponse",vm);
+        }
+    }
+}
